ef730f8d451932c38792f3e80ff74550409d2203
youji <dbwlgna98@naver.com>
2022-04-05 15:09:04 +0900

    initial project

---
+++ src/test/DBConnection.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConnection {
+
+
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	
+	//19c 적용
+	
+	//URL에 전자지갑 위치 변경하여 테스트
+	private static final String url = "";
+	private static final String user = "";
+	private static final String pwd = "";
+	private Connection con;
+	private Statement stmt;
+
+	public void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			System.out.println("url : "+ url);
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+			stmt = con.createStatement();
+			System.out.println("Statement 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}//end try
+	}//end conn..
+
+	
+}
