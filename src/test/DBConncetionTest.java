3ff1bffb46677e3455613e0ac11fb4ea541c01f7
kimyeonsik123 <yearskr@gmail.com>
2022-04-07 21:54:20 +0900

    yeonsikfirstcommit

---
+++ src/test/DBConncetionTest.java
@@ -0,0 +1,16 @@
+package test;
+
+public class DBConncetionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DBConnection db = new DBConnection();
+
+		db.connDB();
+		
+		System.out.println("¼º°ø ");
+		
+	}
+
+}
