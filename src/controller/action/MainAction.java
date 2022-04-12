53ade49ecfe07915e8e36a037681cc344f476a67
wjddp <wjddp@Jeongyeseong>
2022-04-13 03:52:52 +0900

    메인페이지 상품 검색 기능

--- src/controller/action/MainAction.java
+++ src/controller/action/MainAction.java
@@ -16,16 +16,20 @@ public class MainAction implements Action {
   public void execute(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {    
 	  
-	  	String url = "/main.jsp";	 
-	  	CategoryDAO categoryDAO = CategoryDAO.getInstance();
+	  	String url = "/main.jsp";
+	  	
+	  	CategoryDAO categoryDAO = CategoryDAO.getInstance();	 
 	  	ProductDAO productDAO = ProductDAO.getInstance();
-	  		
+	  	
 	  	ArrayList<CategoryVO> cateogoryList = categoryDAO.listCategory();
+	  	
+	  	System.out.println(cateogoryList.toString());
 	  	
 	  	ArrayList<ProductVO> productList = productDAO.productList(2);
 	  	ArrayList<ProductVO> productList2 = productDAO.productList(52);
 	  	
 	    request.setAttribute("categoryList", cateogoryList);
+	    
 	    request.setAttribute("productList", productList);
 	    request.setAttribute("productList2", productList2);
 	    
